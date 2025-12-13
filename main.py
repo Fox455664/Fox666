d2e49932eea19edd4686f762c6ca1682cfa4c6b3
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2025-12-13 16:09:42 +0200

    Refactor bot startup to use asyncio.run
    Refactor the bot startup process to use asyncio.run for better handling of the event loop.

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 # --- START OF FILE main.py ---
 import asyncio
 from pyrogram import Client
-from pytgcalls import idle  # <--- From pytgcalls
 import os
 import sys
 import random
 from bot import start_zombiebot
 from pyromod import listen
 
-loop = asyncio.get_event_loop()
+async def main():
+    await start_zombiebot()
+    print("Bot is running...")
+    await asyncio.Event().wait()  # يخلي البوت شغال دايمًا
 
 if __name__ == "__main__":
     try:
-        loop.run_until_complete(start_zombiebot())
+        asyncio.run(main())
     except KeyboardInterrupt:
         print("Bot stopped by user.")
     except Exception as e:
