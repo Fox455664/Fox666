64e12c6261b3fefd4d3c3e7c6318b9eddb162b35
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2025-12-13 14:07:50 +0200

    Fix formatting and update environment variable comments

--- casery.py
+++ casery.py
@@ -39,5 +39,6 @@ photosource = os.getenv("SOURCE_PHOTO_URL", "https://envs.sh/ws4.webp")
 bot_token = os.getenv("BOT_TOKEN", "8400587535:AAFQyiZETQxYJgCQhXOzESv3pibI4vwQ4I0")
 
 ## جلسه الاشتراك (Helper Account Session String)
-bot_token2 = os.getenv("SESSION_STRING", "BAF5OpQAcC63VzQzOLP6X47PtfKRy5cyy_734-crPdorMVBFipR59RF43hjtx3WPdw7bCkqF7xt472bIcGi1PVdCfmwex26JLZ043brofbGNngyngoa_UZ4T3qhs0v_Pqa2tguXo1SCRi_rrDIsPmoVQkvB4S9QeCAwYovyiEoMm2RVSMMpXo3eht0l6-387RLWis_yIWTWPgH1p6tzAVYoxZCFebQvvzeKCBg8Mo8Ibs2BwvvEsyZknCh7OgbiRCHrDpHPEkNoGa-vAZkwS8GC7m9iM4oqvTq_jlDqELNMjsx0PGlyOYzfjSPKsYkRPozPd_4v4GUusjV-bVJpfuiWsu8p2fgAAAAHx6C_MAA"
+bot_token2 = os.getenv("SESSION_STRING", "BAF5OpQAcC63VzQzOLP6X47PtfKRy5cyy_734-crPdorMVBFipR59RF43hjtx3WPdw7bCkqF7xt472bIcGi1PVdCfmwex26JLZ043brofbGNngyngoa_UZ4T3qhs0v_Pqa2tguXo1SCRi_rrDIsPmoVQkvB4S9QeCAwYovyiEoMm2RVSMMpXo3eht0l6-387RLWis_yIWTWPgH1p6tzAVYoxZCFebQvvzeKCBg8Mo8Ibs2BwvvEsyZknCh7OgbiRCHrDpHPEkNoGa-vAZkwS8GC7m9iM4oqvTq_jlDqELNMjsx0PGlyOYzfjSPKsYkRPozPd_4v4GUusjV-bVJpfuiWsu8p2fgAAAAHx6C_MAA")
 # --- END OF FILE casery.py ---
+
