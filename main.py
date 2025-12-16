aad845f546c9d44ea6e088bbf1dc94b11eaf0913
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2025-12-16 07:18:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,39 @@ import sys
 import logging
 from bot import start_zombiebot
 
+# ================= Flask =================
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running"
+
+def run_flask():
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
+
+# =========================================
+
 # إعداد السجلات
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("Main")
 
 async def main():
     logger.info("Initializing system...")
+
+    # تشغيل Flask في Thread منفصل
+    flask_thread = threading.Thread(target=run_flask)
+    flask_thread.daemon = True
+    flask_thread.start()
+
     try:
         await start_zombiebot()
     except Exception as e:
         logger.error(f"CRITICAL ERROR in main loop: {e}", exc_info=True)
-    
+
     logger.info("Service is running. Press Ctrl+C to stop.")
     await asyncio.Event().wait()
 
